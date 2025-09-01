c63a723ad29509ce3541f6674c82ad2ef09d9924
Anupama06-collab <anupamamolahalli@gmail.com>
2025-09-01 21:35:01 +0530

    updated

--- frontend/src/pages/Shop.jsx
+++ frontend/src/pages/Shop.jsx
@@ -9,7 +9,7 @@ const Shop = () => {
   useEffect(() => {
     const fetchProducts = async () => {
       try {
-        const res = await axios.get("http://localhost:5000/api/products");
+        const res = await axios.get("https://e-commerce-uxbe.onrender.com/api/products");
         setProducts(res.data);
       } catch (err) {
         console.error("Error fetching products:", err);
