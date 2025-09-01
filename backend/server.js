9188862355434fa529c81ef7d2dfdfdc85d9e8f8
Anupama06-collab <anupamamolahalli@gmail.com>
2025-09-01 13:39:55 +0530

    Updated commit

--- backend/server.js
+++ backend/server.js
@@ -13,10 +13,8 @@ app.use(express.json());
 app.use('/api/products', productRoutes);
 
 // Connect to MongoDB
-mongoose.connect('mongodb://127.0.0.1:27017/ecommerce', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-})
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+
 .then(() => {
   console.log('MongoDB connected');
   app.listen(5000, () => console.log('Server running on port 5000'));
